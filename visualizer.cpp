e691a10867347684019abaf6443b6c7a220cee43
feusx <feusuaiga@gmail.com>
2025-09-12 18:54:22 +0300

    huge update

--- visualizer.cpp
+++ visualizer.cpp
@@ -1,85 +1,93 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <random>
-#include <ranges>
-#include <algorithm>
-#include <climits>
+#include <vector>
 using namespace std;
 
-void drawState(vector<int>& vec, SDL_Renderer* renderer, uint16_t red, uint16_t blue, int range_end);
-void selectionSort(vector<int>& vec, SDL_Renderer* renderer, int range_end);
+const int WINDOW_WIDTH = 1024;
+const int WINDOW_HEIGHT = 768;
+
+void drawState(const vector<int>& vec, SDL_Renderer* renderer, int highlight_1, int highlight_2, int max_value, int stripe_width);
+void selectionSort(vector<int>& vec, SDL_Renderer* renderer, int max_value, int stripe_width);
 
 int main()
 {
-  int range_end = 99;
-  int range_start = 1;
-  random_device rd;
-  uniform_int_distribution d(range_start, range_end);
-  vector<int> vec;
+  int num_elements, range_start, range_end;
+  cout << "Enter number of elements to sort: ";
+  cin >> num_elements;
 
-  /*cout << "Enter the range start: ";
+  cout << "Enter the range start: ";
   cin >> range_start;
-  cout << "Enter the range end: ";
-  cin >> range_end;*/
 
-  if(range_end < range_start)
-  {
+  cout << "Enter the range end: ";
+  cin >> range_end;
+
+  if (range_end < range_start)
     swap(range_end, range_start);
-  }
-  
-  for(int i = 0; i < range_end; i++)
-  {
-    vec.push_back(d(rd));
-  }
+
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<int> dist(range_start, range_end);
+
+  vector<int> vec;
+  for (int i = 0; i < num_elements; ++i)
+    vec.push_back(dist(gen));
 
   SDL_Window* window = nullptr;
   SDL_Renderer* renderer = nullptr;
-  SDL_CreateWindowAndRenderer(100*10, 100*10, 0, &window, &renderer);
-  SDL_RenderSetScale(renderer, 10, 10);
+  SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
 
-  selectionSort(vec, renderer, range_end);
+  int stripe_width = WINDOW_WIDTH / num_elements;
+
+  selectionSort(vec, renderer, range_end, stripe_width);
+
+  SDL_Delay(2000);
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 
   return 0;
 }
 
-
-void drawState(vector<int>& vec, SDL_Renderer* renderer, uint16_t red, uint16_t blue, int range_end)
+void drawState(const vector<int>& vec, SDL_Renderer* renderer, int highlight_1, int highlight_2, int max_value, int stripe_width)
 {
-  int index = 0;
-  for(int i : vec)
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  for (int i = 0; i < vec.size(); i++)
   {
-    if(index == red)
+    if (i == highlight_1)
       SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    else if(index == blue)
+    else if (i == highlight_2)
       SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     else
       SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    
-    SDL_RenderDrawLine(renderer, index, range_end, index, i);
-    index += 1;
+
+    int scaled_height = (static_cast<float>(vec[i]) / max_value) * WINDOW_HEIGHT;
+    int x = i * stripe_width;
+    SDL_Rect bar = { x, WINDOW_HEIGHT - scaled_height, stripe_width, scaled_height };
+    SDL_RenderFillRect(renderer, &bar);
   }
+
+  SDL_RenderPresent(renderer);
 }
 
-void selectionSort(vector<int>& vec, SDL_Renderer* renderer, int range_end)
+void selectionSort(vector<int>& vec, SDL_Renderer* renderer, int max_value, int stripe_width)
 {
-  for(uint16_t i = 0; i < vec.size(); i++)
+  for (int i = 0; i < vec.size(); i++)
   {
-    uint16_t min_index = i;
-    
-    for(uint16_t j = i + 1; j < vec.size(); j++)
+    int min_index = i;
+    for (int j = i + 1; j < vec.size(); j++)
     {
-      if(vec[j] < vec[min_index])
+      if (vec[j] < vec[min_index])
         min_index = j;
-     
     }
-    if(min_index != i) swap(vec[i], vec[min_index]);
-     
-    // clear the screen
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
 
-    drawState(vec, renderer, i, min_index, range_end);  
-    SDL_RenderPresent(renderer);
-    SDL_Delay(100);
+    if (min_index != i)
+      swap(vec[i], vec[min_index]);
+
+    drawState(vec, renderer, i, min_index, max_value, stripe_width);
+    SDL_Delay(20);
   }
 }
