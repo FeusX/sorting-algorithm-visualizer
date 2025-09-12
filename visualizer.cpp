825f4612ac54c9e360338f876122c70ebc91694b
feusx <feusuaiga@gmail.com>
2025-09-12 19:48:27 +0300

    this doesn't work as intended but i will fix later

--- visualizer.cpp
+++ visualizer.cpp
@@ -9,9 +9,16 @@ const int WINDOW_HEIGHT = 768;
 
 void drawState(const vector<int>& vec, SDL_Renderer* renderer, int highlight_1, int highlight_2, int max_value, int stripe_width);
 void selectionSort(vector<int>& vec, SDL_Renderer* renderer, int max_value, int stripe_width);
+void bubbleSort(vector <int>& vec, SDL_Renderer* renderer, int max_value, int stripe_width);
 
 int main()
 {
+
+if (SDL_Init(SDL_INIT_VIDEO) < 0) 
+{
+  cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
+  return 1;
+}
   int num_elements, range_start, range_end;
   cout << "Enter number of elements to sort: ";
   cin >> num_elements;
@@ -22,7 +29,7 @@ int main()
   cout << "Enter the range end: ";
   cin >> range_end;
 
-  if (range_end < range_start)
+  if(range_end < range_start)
     swap(range_end, range_start);
 
   random_device rd;
@@ -30,16 +37,35 @@ int main()
   uniform_int_distribution<int> dist(range_start, range_end);
 
   vector<int> vec;
-  for (int i = 0; i < num_elements; ++i)
+  for(int i = 0; i < num_elements; ++i)
     vec.push_back(dist(gen));
+
+  /*SDL_Window* window = nullptr;
+  SDL_Renderer* renderer = nullptr;
+  SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);*/
+
+  int stripe_width = WINDOW_WIDTH / num_elements;
+
+  uint8_t algorithm_number;
+  cout << "0-Bubble Sort, 1-Selection Sort" << endl;
+  cout << "Enter the algorithm that you want to visualize: ";
+  cin >> algorithm_number;
 
   SDL_Window* window = nullptr;
   SDL_Renderer* renderer = nullptr;
   SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
 
-  int stripe_width = WINDOW_WIDTH / num_elements;
-
-  selectionSort(vec, renderer, range_end, stripe_width);
+  switch (algorithm_number)
+  {
+    case 0:
+      bubbleSort(vec, renderer, range_end, stripe_width);
+      break;
+    case 1:
+      selectionSort(vec, renderer, range_end, stripe_width);
+      break;
+    default:
+      break;
+  }
 
   SDL_Delay(2000);
 
@@ -55,11 +81,11 @@ void drawState(const vector<int>& vec, SDL_Renderer* renderer, int highlight_1, 
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
 
-  for (int i = 0; i < vec.size(); i++)
+  for(int i = 0; i < vec.size(); i++)
   {
-    if (i == highlight_1)
+    if(i == highlight_1)
       SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    else if (i == highlight_2)
+    else if(i == highlight_2)
       SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     else
       SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
@@ -75,19 +101,40 @@ void drawState(const vector<int>& vec, SDL_Renderer* renderer, int highlight_1, 
 
 void selectionSort(vector<int>& vec, SDL_Renderer* renderer, int max_value, int stripe_width)
 {
-  for (int i = 0; i < vec.size(); i++)
+  for(int i = 0; i < vec.size(); i++)
   {
     int min_index = i;
-    for (int j = i + 1; j < vec.size(); j++)
+    for(int j = i + 1; j < vec.size(); j++)
     {
-      if (vec[j] < vec[min_index])
+      if(vec[j] < vec[min_index])
         min_index = j;
     }
 
-    if (min_index != i)
+    if(min_index != i)
       swap(vec[i], vec[min_index]);
 
     drawState(vec, renderer, i, min_index, max_value, stripe_width);
-    SDL_Delay(20);
+    SDL_Delay(10);
   }
 }
+
+void bubbleSort(vector <int>& vec, SDL_Renderer* renderer, int max_value, int stripe_width)
+{
+  bool swapped;
+  int i = vec.size();
+  do
+  {
+    swapped = false;
+    for(int j = 0; j < vec.size() - 1; j++)
+    {
+      if(vec[j] > vec[j + 1])
+      {
+        swap(vec[j], vec[j + 1]);
+        swapped = true;
+      }
+      drawState(vec, renderer, j, j + 1, max_value, stripe_width);
+      SDL_Delay(10);
+    }
+    i--;
+  }while(swapped);  
+} 
